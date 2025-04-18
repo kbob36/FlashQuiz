17adccbe16097a04ce6dc78933a78140e0c9b0c8
kbob36 <venterkian@gmail.com>
2025-04-18 14:07:03 -0400

    This is the first changes to the project, I layed out some todo items to think about in the future, but mainly its been a while since I've coded with python so I am trying to get back up to speed, I've layed the groud works for a flash card object and am looking forward to working on this in the future.

--- main.py
+++ main.py
@@ -1,3 +1,41 @@
 """
-This is a test
+Study Project
+By: Kian Venter
+Created:2025-04-18
+
+TODO: 
+Practice functions and objects
+Find UI Options
+Learn AI API
+Start Developping Framework
+
 """
+
+"""
+Class used for all flash card objects
+
+The idea here is to create an object for each flashcard that the student
+can add information too
+"""
+class Flash_Card:
+    def __init__(self):
+        self.input = None
+        self.output = None
+
+    def set_input(self, input):
+        self.input = input
+
+    def set_output(self, output):
+        self.output = output
+
+def main():
+    #Create an object
+    Set_1 = Flash_Card()
+    Set_1.set_input("2+2") #Modify its attributes with personal functions
+    Set_1.set_output("4")
+
+    #print attributes to console
+    print(f"The input is {Set_1.input} and output is {Set_1.output}")
+
+if __name__ == "__main__":
+    main()
